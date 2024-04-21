bc9ea99877eccfac197ca96c4b5346359e9ff9a9
박철현 <tghy4@naver.com>
2024-04-21 21:55:07 +0900

    Login 작업을 위한 초기 세팅 완료

--- main/java/hello/login/web/member/MemberController.java
+++ main/java/hello/login/web/member/MemberController.java
@@ -3,15 +3,16 @@ package hello.login.web.member;
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/members")
@@ -19,14 +20,14 @@ public class MemberController {
     private final MemberRepository repository;
 
     @GetMapping("/add")
-    public String addForm(Model model) {
-        model.addAttribute("member", new Member());
+    public String addForm(Member member) {
         return "members/addMemberForm";
     }
 
     @PostMapping("/add")
     public String save(@Validated Member member, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
+            log.info("LogIn Errors : {}", bindingResult);
             return "members/addMemberForm";
         }
         repository.save(member);
