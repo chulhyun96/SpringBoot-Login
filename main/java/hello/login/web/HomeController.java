98a9ba5eb0d45dd150b5a108516bbdd3133c25cd
박철현 <tghy4@naver.com>
2024-04-23 23:08:24 +0900

    Cookie를 세팅하고, Cookie 값을 통해 로그인 및 로그아웃 관리

--- main/java/hello/login/web/HomeController.java
+++ main/java/hello/login/web/HomeController.java
@@ -1,15 +1,57 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final MemberRepository repository;
 
-    @GetMapping("/")
+    /*@GetMapping("/")*/
     public String home() {
         return "home";
+    }
+    
+    @GetMapping("/")
+    // 로그인 안한 사용자도 들어와야 하기 때문에 required=false
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        //로그인을 안한 상태 일 경우
+
+        if(memberId == null) {
+            return "home";
+        }
+
+        //로그인
+        Member loginMember = repository.findById(memberId);
+        //DB에 없을 수도 있음. 쿠키가 너무 옛날에 만들어졌거나해서
+        if (loginMember == null) {
+            return "home";
+        }
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response,"memberId");
+        return "redirect:/";
+    }
+
+    private static void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
--- main/java/hello/login/web/login/LoginController.java
+++ main/java/hello/login/web/login/LoginController.java
@@ -4,6 +4,7 @@ import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,19 +12,24 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class LoginController {
     private final LoginService loginService;
+    private final HttpServletResponse httpServletResponse;
 
     @GetMapping("/login")
-    public String loginForm(LoginForm loginForm) {
+    public String loginForm(Model model) {
+        model.addAttribute("loginForm", new LoginForm());
         return "login/loginForm";
     }
 
     @PostMapping("/login")
-    public String login(@Validated LoginForm form, BindingResult bindingResult) {
+    public String login(@Validated LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             log.info("LogIn form error: {}", bindingResult);
         }
@@ -32,6 +38,11 @@ public class LoginController {
             bindingResult.reject("loginFail", "Invalid loginId or password");
             return "login/loginForm";
         }
+        // 로그인 성공 처리
+        // 쿠키에 시간 정보를 주지 않으면 세션 쿠키 이다(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(member.getId()));
+        response.addCookie(idCookie);
+        log.info("Login Success");
         return "redirect:/";
     }
 }
